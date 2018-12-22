d8ef6a05b47fb541f751a49593453f1428a86eeb
Niels Bergwerff <niels@bergwerff.info>
2018-12-22 15:53:37 +0100

    Het begin is er.
    Het probleempje dat ik had is opgelost.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 //Als het document geladen is:
-$(document).ready(() => {
-      //Variabelen
-      const $menu = $('.menu');
-      const $header = $('.header');
-      const $content = $('.content');
-      const $container = $('.container');
-      const $menuButton = $('#menuButton')
+$(document).ready(function() {
+  //Variabelen
+  const $menu = $(".menu");
+  const $header = $(".header");
+  const $content = $(".content");
+  const $container = $(".container");
+  const $menuButton = $(".menuButton")
 
-      //Als menuknop wordt ingedrukt:
-      $menuButton.click(function() {
-          if (!$container.attr('id', 'menuActive')) {
-              $container.attr('id', 'menuActive');
-            } else {
-              $container.removeAttr('id', 'menuActive');
-            }
-            $menu.slideToggle('slow');
-          });
-      });
+  $menuButton.click(function() {
+    if ($container.hasClass("menuActive")) {
+      $menu.toggle();
+      $container.removeClass("menuActive");
+      $('#menuButtonMenu').toggle();
+      $('#menuButtonHeader').toggle();
+    } else {
+      $menu.toggle();
+      $container.addClass("menuActive");
+      $('#menuButtonMenu').toggle();
+      $('#menuButtonHeader').toggle();
+    }
+  });
+});
