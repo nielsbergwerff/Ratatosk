4d63b6e45ad829b67b2bc8547be1621a28222ed2
Niels Bergwerff <niels@bergwerff.info>
2018-12-24 20:49:42 +0100

    klik ipv mouseleave
    Als je nu naast het menu klikt verdwijnt het menu ipv als de muis het menu verlaat.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,30 @@ $(document).ready(function() {
   //Menu openen en sluiten
   var $menu = $(".menu");
   var $menuButton = $(".menuButton");
-  var $menuButtonHeader = $("#menuButtonHeader");
+  var $menuButton = $(".menuButton");
+
   $menuButton.click(function() {
-    $menu.toggle("slide", {direction: 'right'});
-    if ($menuButtonHeader.css("right") == "0px") {
-      $menuButtonHeader.animate({"right":"20%"});
+    if ($menu.css("visibility") == "hidden") {
+      $menu.animate({"right": "0"});
+      $menu.css("visibility", "visible");
     } else {
-      $menuButtonHeader.animate({"right":"0"});
+      $.when($menu.animate({"right": "-20vw"})).then(function() {
+      $menu.css("visibility", "hidden")});
+    }
+
+    if ($menuButton.css("right") == "20vw") {
+      $menuButton.animate({"right": "40vw"});
+    } else {
+      $menuButton.animate({"right": "20vw"});
     }
   });
-  $menu.mouseleave(function() {
-    $menu.hide("slide", {direction: 'right'});
-    $menuButtonHeader.animate({"right":"0"});
+
+  $(document).mouseup(function(e) {
+    if(!$menu.is(e.target) && $menu.has(e.target).length === 0) {
+      $menuButton.animate({"right": "20vw"});
+      $.when($menu.animate({"right": "-20vw"})).then(function() {
+      $menu.css("visibility", "hidden")});
+    }
   });
 
   //Groepmenu openen en sluiten
