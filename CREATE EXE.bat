0ea92bf7090a066c8ac24edd2d81d001e303a6f7
StriderOfTheWest <ntvangoor@gmail.com>
2019-02-03 20:41:59 +0100

    Onnodige bestanden komen niet in de EXE

--- CREATE EXE.bat
+++ CREATE EXE.bat
@@ -1,2 +1,8 @@
-npm run dist
-makensis InstallerScript.nsi
+call npm run dist
+cd dist\Ratatosk-0.0.1-win-x86
+del "CREATE EXE.bat"
+del "InstallerScript.nsi"
+del "README.md"
+del "Ratatosk Installer.exe"
+del "server.js"
+makensis ..\..\InstallerScript.nsi
