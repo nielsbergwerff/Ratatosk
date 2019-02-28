205dae820db5b6e613bdc72686797dfff4ece31a
StriderOfTheWest <ntvangoor@gmail.com>
2019-02-28 21:02:36 +0100

    Betere notatie, bestanden verplaatst, package.json wordt geëdit

--- CREATE EXE.bat
+++ CREATE EXE.bat
@@ -1,8 +1,22 @@
-call npm run dist
-cd dist\Ratatosk-0.0.1-win-x86
-del "CREATE EXE.bat"
-del "InstallerScript.nsi"
-del "README.md"
-del "Ratatosk Installer.exe"
-rmdir /s /q "js-server"
-makensis ..\..\InstallerScript.nsi
+CALL npm run dist
+
+SETLOCAL=ENABLEDELAYEDEXPANSION
+
+CD dist\Ratatosk-0.0.1-win-x86
+
+POWERSHELL -Command "(gc package.json) -replace 'html/index.html', 'index.html' | Out-File package.json"
+
+DEL "CREATE EXE.bat"
+DEL "Run Server.bat"
+DEL "InstallerScript.nsi"
+DEL "README.md"
+DEL "config.json"
+DEL "Ratatosk Installer.exe"
+
+REN "js-client" "js"
+MOVE /y html\*.* .
+
+RD /s /q "html"
+RD /s /q "js-server"
+
+MAKENSIS ..\..\InstallerScript.nsi
