6c3ab0295e5abe3cbc81b3459eee1510b27e9630
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-07 17:47:47 +0100

    Updated

--- INSTALL SERVER.bat
+++ INSTALL SERVER.bat
@@ -1,11 +1,11 @@
 MD js-server\models
 COPY NUL js-server\models\users.js
 
-CALL npm install express
-CALL npm install socket.io
-CALL npm install body-parser
-CALL npm install helmet
-CALL npm install mysql
-CALL npm install sequelize-auto
-CALL npm install bcrypt
-CALL npm install cookie-session
+CALL npm i express
+CALL npm i socket.io
+CALL npm i body-parser
+CALL npm i helmet
+CALL npm i mysql
+CALL npm i sequelize-auto
+CALL npm i cookie-session
+CALL npm i morgan
