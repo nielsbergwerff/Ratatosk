2415c317bfe086b44ca3d93f5d4661debd683630
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-07 15:44:38 +0100

    Updated for further sequelize integration; NOT FINISHED

--- js-server/db.js
+++ js-server/db.js
@@ -1,57 +1,48 @@
 const mysql = require('mysql');
 const config = require('../config.json');
-const SequelizeAuto = require('sequelize-auto')
+const SequelizeAuto = require('sequelize-auto');
+const Sequelize = require('sequelize');
 const auto = new SequelizeAuto(config.database,config.user,config.password);
 
 auto.run(function (err) {
   if (err) throw err;
+  else {
+    const sequelize = new Sequelize(config.database, config.user, config.password, {
+      host: config.host,
+      dialect: 'mysql',
+      operatorsAliases: false,
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    });
+
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log('Connection has been established successfully.');
+      })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+  }
 });
 
 var exports = module.exports = {};
 
-var con = mysql.createConnection({
-  host: config.host,
-  user: config.user,
-  password: config.password,
-  database: config.database
-});
-
-con.connect((err)=>{
-  if(err) throw "Server connection error";
-  else console.log("Connected to database");
-});
-
-function query(sql,cb){
-  con.query(sql,(err,result)=>{
-    if(result[0])cb(result);
-    else cb(null);
-  });
-}
-
-module.exports.query = query;
-
-const UserModel = require('./models/users');
-const Sequelize = require('sequelize');
+const User = require('./models/users');
 const bcrypt = require('bcrypt');
 
 // create a sequelize instance with our local mysql database information.
-const sequelize = new Sequelize(config.database, config.user, config.password, {
-  host: config.host,
-  dialect: 'mysql',
-  operatorsAliases: false,
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+function login(user,passUser.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
