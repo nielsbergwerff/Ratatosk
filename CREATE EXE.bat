84e98d5b133d5f4302ae24bbdb293313768fe0d2
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-07 15:40:53 +0100

    Updated; Edits index.html to point to socket.io.js

--- CREATE EXE.bat
+++ CREATE EXE.bat
@@ -1,22 +1,14 @@
 CALL npm install nwjs-builder-phoenix -D
 CALL npm run dist
 
-RD /s /q .\dist\Ratatosk-win-x86
-MOVE .\dist\Ratatosk-1.0.0-win-x86 .\dist\Ratatosk-win-x86
-CD dist\Ratatosk-win-x86 || exit /b
-
-POWERSHELL -Command "(gc package.json) -replace 'html/index.html', 'index.html' | Out-File package.json" -Encoding UTF8
-DEL "CREATE EXE.bat"
-DEL "RUN SERVER.bat"
-DEL "InstallerScript.nsi"
-DEL "README.md"
-DEL "config.json"
-DEL "Ratatosk Installer.exe"
+CD dist\Ratatosk-win || exit /b
 
 REN "js-client" "js"
 MOVE /y html\*.* .
 
 RD /s /q "html"
-RD /s /q "js-server"
+
+POWERSHELL -Command "(gc package.json) -replace 'html/index.html', 'index.html' | Out-File package.json -Encoding UTF8"
+POWERSHELL -Command "(gc index.html) -replace '/socket.io/socket.io.js', './node_modules/socket.io-client/dist/socket.io.js' | Out-File index.html -Encoding UTF8"
 
 MAKENSIS ..\..\InstallerScript.nsi
