0c37685e5860bed0f3e22ed4d612e14679c37bd2
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-09 01:13:56 +0100

    findUser() to verify users in db

--- js-server/db.js
+++ js-server/db.js
@@ -1,56 +1,62 @@
-const mysql = require('mysql');
-const config = require('../config.json');
-const SequelizeAuto = require('sequelize-auto');
-const Sequelize = require('sequelize');
-const auto = new SequelizeAuto(config.database,config.user,config.password);
+module.exports = function() {
+const mysql = require('mysql'),
+      config = require('../config.json'),
+      SequelizeAuto = require('sequelize-auto'),
+      Sequelize = require('sequelize'),
+      auto = new SequelizeAuto(config.database,config.user,config.password, {
+        additional:{
+          timestamps: false
+        }
+      });
 
 auto.run(function (err) {
-  if (err) throw err;
-  else {
-    const sequelize = new Sequelize(config.database, config.user, config.password, {
-      host: config.host,
-      dialect: 'mysql',
-      operatorsAliases: false,
-      pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-      }
-    });
+    if (err) throw err;
+//all following code is wrapped within this function
+//no indents for my sanity
 
-    sequelize
-      .authenticate()
-      .then(() => {
-        console.log('Connection has been established successfully.');
-      })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    });
+var sequelize = new Sequelize(config.database, config.user, config.password, {
+  host: config.host,
+  dialect: 'mysql',
+  operatorsAliases: false,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
-var exports = module.exports = {};
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 
 const Gebruikers = require('./models/gebruikers');
 const Berichten = require('./models/berichten');
 const Groepen = require('./models/groepen');
 const bcrypt = require('bcrypt');
 
-// create a sequelize instance with our local mysql database information.
-/*function login(user,passUser.findOne({ where: { username: username } }).then(function (user) {
-            if (!user) {
-                res.redirect('/login');
-            } else if (!user.validPassword(password)) {
-                res.redirect('/login');
-            } else {
-                req.session.user = user.dataValues;
-                res.redirect('/dashboard');
-            }
-        });
-*/
 function getGroupList(user){
   Gebruikers.findOne({where:{ID:user}}).then(user=>{
     return user.ID;
   })
 }
+
+// create a sequelize instance with our local mysql database information.
+function findUser(user,pass,cb){
+  User.findOne({ where: { username: user, password: pass} }).then(user=>{
+    if(user)cb(true);
+    else cb(false);
+  });
+}
+
+exports.sequelize = sequelize;
+exports.findUser = findUser;
+
+//end of auto.run
+});
+};
