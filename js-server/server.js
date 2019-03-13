a03bb7f4c838e83bed97a46f4637268c2a292d30
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-13 16:55:12 +0100

    express-session/socket.io integratie

--- js-server/server.js
+++ js-server/server.js
@@ -10,29 +10,24 @@ const express = require('express'),
       cookieParser = require('cookie-parser'),
       expressSession = require('express-session'),
       connectSessionSequelize = require('connect-session-sequelize'),
+      db = require('./db'),
       config = require('../config.json');
 
-//create the session with random secret
-//function {
-  const db = require('./db')();
-  //r();
-//});
 
-//dbPromise.then(()=>{
-  const session = expressSession({
-    key: 'user_sid',
-    secret: 'ns&10Kd9;',
-  /*  store: new (connectSessionSequelize(expressSession.Store))({
-      db: db.sequelize
-    }),
-    */resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      //secure: true
-      expires: 60 * 60 * 1000
-    }
-});//});
+const session = expressSession({
+  key: 'user_sid',
+  secret: 'ns&10Kd9;',
+  store: new (connectSessionSequelize(expressSession.Store))({
+    db: db.sequelize
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    //secure: true
+    expires: 60 * 60 * 1000
+  }
+});
 
 const port = 80;
 
@@ -52,6 +47,10 @@ app.use((req, res, next) => {
   next();
 });
 
+io.use(function(socket, next) {
+    session(socket.request, socket.request.res, next);
+});
+
 http.listen(port,()=>{
   console.log(`listening on *:${port}`);
 });
@@ -61,26 +60,20 @@ io.on('connection',(socket)=>{
   io.emit('set group list',db.getGroupList(socket.request.session.user));
 
   socket.on('set group',(group)=>{
-    session(req,res,()=>{
-      req.session.group = group;
-
-      io.emit('set group',req.session.group);
-    });
+    socket.request.session.group = group;
+    io.emit('set group',socket.request.session.group);
   });
 
   socket.on('chat message',(msg)=>{
-    session(req,res,()=>{
-        io.emit('chat message',req.session.username+": "+msg); // Do something with req.session
-    });
+      io.emit('chat message',socket.request.session.user+": "+msg); // Do something with req.session
   });
 
   socket.on('add group',(data)=>{});
 });
 
 app.get('/',(req,res)=>{
-  /*if(req.session.loggedIn==='true')*/res.sendFile('index.html',{root:'../html'});
-  //else res.redirect('/login');
-  req.session.user = "root";
+  if(req.session.loggedIn==='true')res.sendFile('index.html',{root:'../html'});
+  else res.redirect('/login');
 });
 
 app.route('/login')
@@ -95,7 +88,7 @@ app.route('/login')
     db.findUser(username,hash,(result)=>{
       if(result){
         req.session.loggedIn = 'true';
-        req.session.username = username;
+        req.session.user = username;
         //req.session.group = result[0].latestGroup;
         res.redirect('/');
       } else res.redirect('/login');
