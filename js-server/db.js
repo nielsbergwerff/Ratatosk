f02f5905ba255b0c2fa8b1d0c8f5d7385c4edbbe
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-21 23:58:16 +0100

    functies voor verkrijgen van groepen, berichten en het opslaan van berichten

--- js-server/db.js
+++ js-server/db.js
@@ -17,7 +17,7 @@ var runSync = function() {
 }();
 
 var sequelize = new Sequelize(config.database, config.user, config.password, {
-  host: config.host,
+  host: config.databaseHost,
   dialect: 'mysql',
   pool: {
     max: 10,
@@ -41,43 +41,95 @@ const Gebruikers = sequelize.import('./models/gebruikers'),
       Groepen = sequelize.import('./models/groepen');
       GroepsLeden = sequelize.import('./models/groepsleden');
 
-//Gebruikers.hasMany(Berichten);
-//Berichten.belongsTo(Gebruikers);
-
-//Gebruikers.hasMany(GroepsLeden);
-//GroepsLeden.belongsTo(Gebruikers);
-
-Groepen.hasMany(GroepsLeden);
-GroepsLeden.belongsTo(Groepen);
-
-//Groepen.hasMany(Berichten);
-//Berichten.belongsTo(Groepen);
+Gebruikers.hasMany(Berichten,{foreignKey: 'AuteursID'});
+Berichten.belongsTo(Gebruikers,{foreignKey: 'AuteursID'});
+Gebruikers.belongsToMany(Groepen, {through: GroepsLeden, foreignKey: 'GebruikersID'});
+Groepen.belongsToMany(Gebruikers, {through: GroepsLeden, foreignKey: 'GroepsID'});
+Groepen.hasMany(Berichten,{foreignKey: 'GroepsID',as: 'berichten'});
+Berichten.belongsTo(Groepen,{foreignKey: 'GroepsID',as: 'berichten'});
 
 function getGroupList( user, cb ){
 
-  var i, output = [];
+  var i,j, output = [];
 
-  Groepen.findAll( {
-    include:[
+  Groepen.findAll({
+    attributes: ['ID','Naam'],
+    include: [
       {
-        model: GroepsLeden
+        model: Gebruikers,
+        attributes: [],
+        where: { ID: user }
+      },
+      {
+        limit:1,
+        model: Berichten,
+        as: 'berichten',
+        attributes: ['Tijd'],
+        order: [[ 'Tijd', 'DESC' ]]
       }
     ]
   })
     .then( groups => {
-      for(i=0;i<groups.length;i++) output[i] = [groups[i].dataValues.ID,groups[i].dataValues.Naam];
-      cb(output);
+      for(i=0;i<groups.length;i++) output[i] = groups[i];
+
+      Groepen.findAll({
+        where: {EigenaarsID: user},
+        attributes: ["ID","Naam"],
+
+        include: [
+          {
+            limit: 1,
+            model: Berichten,
+            as: 'berichten',
+            attributes: ['Tijd'],
+            order: [[ 'Tijd', 'DESC' ]]
+          }
+        ]
+      })
+        .then( groups => {
+          for(j=0;j<groups.length;j++) output[i+j] = groups[j];
+          groups.sort((a,b)=>{
+            if(a.berichten[0]&&b.berichten[0]&&a.berichten[0].Tijd>b.berichten[0].Tijd)return -1;
+            else return 0;
+          });
+          cb(output);
+        });
     });
 }
 
 // create a sequelize instance with our local mysql database information.
 function findUser(user,pass,cb){
-  Gebruikers.findOne({ where: { ID  : user, Wachtwoord: pass} }).then(user=>{
+  Gebruikers.findOne({ where: { ID: user, Wachtwoord: pass}}).then(user=>{
     if(user)cb(true);
     else cb(false);
   });
 }
 
+function getGroupMessages(group,cb){
+  Berichten.findAll({
+    include: [
+      {
+        model: Groepen,
+        where: { ID: group }
+      }
+    ]
+  }).
+    then(berichten=>{
+      cb(berichten)
+    })
+}
+
+function saveMessage(text,group,user,cb){
+  Berichten.create({
+    Bericht: text,
+    AuteursID: user,
+    GroepsID: group
+  });
+  cb(false)
+}
+
 exports.sequelize = sequelize;
 exports.findUser = findUser;
 exports.getGroupList = getGroupList;
+exports.getGroupMessages = getGroupMessages;
+exports.saveMessage = saveMessage;
