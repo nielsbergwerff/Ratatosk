7e3482d2247ed514dcea35af5de4793f48576b55
StriderOfTheWest <ntvangoor@gmail.com>
2019-03-21 23:53:13 +0100

    Code opgesplitst voor overzicht

---
+++ js-server/io.js
@@ -0,0 +1,27 @@
+module.exports = function(io,db){
+io.on('connection',(socket)=>{
+
+  db.getGroupList(socket.request.session.user,list=>{
+    io.emit('get group list',list);
+  });
+
+  socket.on('get messages',(group)=>{
+    db.getGroupMessages(group,output=>{
+      io.emit('get messages', output);
+    });
+  });
+
+  socket.on('set group',(group)=>{
+    socket.request.session.group = group;
+    io.emit('set group',group);
+  });
+
+  socket.on('chat message',(msg)=>{
+    db.saveMessage(msg,socket.request.session.group.ID,socket.request.session.user,(err)=>{
+      if(!err)io.emit('chat message',socket.request.session.user+": "+msg);
+    });
+  });
+
+  socket.on('add group',(data)=>{});
+});
+}
