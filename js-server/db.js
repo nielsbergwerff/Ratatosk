a59d39459f5c020a62af8747cc18c09cf319bace
StriderOfTheWest <ntvangoor@gmail.com>
2019-04-04 21:52:48 +0200

    method voor member lijst; method voor groep verwijderen

--- js-server/db.js
+++ js-server/db.js
@@ -8,15 +8,15 @@ const config = require('../config.json'),
         additional:{
           timestamps: false
         }
-      });
+      })
 
-//const Op = Sequelize.Op;
+//const Op = Sequelize.Op
 
 var runSync = function() {
-  var done = false;
-  auto.run(()=>{done = true;});
-  deasync.loopWhile(function(){return !done;});
-}();
+  var done = false
+  auto.run(()=>{done = true })
+  deasync.loopWhile(function(){return !done })
+}()
 
 var sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.databaseHost,
@@ -29,33 +29,30 @@ var sequelize = new Sequelize(config.database, config.user, config.password, {
     acquire: 30000,
     idle: 10000
   }
-});
+})
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log('Connection has been established successfully.')
   })
 .catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
+  console.error('Unable to connect to the database:', err)
+})
 
 const Gebruikers = sequelize.import('./models/gebruikers'),
       Berichten = sequelize.import('./models/berichten'),
-      Groepen = sequelize.import('./models/groepen');
-      GroepsLeden = sequelize.import('./models/groepsleden');
+      Groepen = sequelize.import('./models/groepen')
+      GroepsLeden = sequelize.import('./models/groepsleden')
 
-Gebruikers.hasMany(Berichten,{foreignKey: 'AuteursID'});
-Berichten.belongsTo(Gebruikers,{foreignKey: 'AuteursID'});
-Gebruikers.belongsToMany(Groepen, {through: GroepsLeden, foreignKey: 'GebruikersID'});
-Groepen.belongsToMany(Gebruikers, {through: GroepsLeden, foreignKey: 'GroepsID'});
-Groepen.hasMany(Berichten,{foreignKey: 'GroepsID',as: 'berichten'});
-Berichten.belongsTo(Groepen,{foreignKey: 'GroepsID'});
+Gebruikers.hasMany(Berichten,{foreignKey: 'AuteursID'})
+Berichten.belongsTo(Gebruikers,{foreignKey: 'AuteursID'})
+Gebruikers.belongsToMany(Groepen, {through: GroepsLeden, foreignKey: 'GebruikersID'})
+Groepen.belongsToMany(Gebruikers, {through: GroepsLeden, foreignKey: 'GroepsID'})
+Groepen.hasMany(Berichten,{foreignKey: 'GroepsID',as: 'berichten'})
+Berichten.belongsTo(Groepen,{foreignKey: 'GroepsID'})
 
-function getGroupList( user, cb ){
-
-  var i,j, output = [];
-
+function getMemberGroups(user,cb){
   Groepen.findAll({
     attributes: ['ID','Naam'],
     include: [
@@ -73,40 +70,73 @@ function getGroupList( user, cb ){
       }
     ]
   })
-    .then( groups => {
-      for(i=0;i<groups.length;i++) output[i] = groups[i];
+    .then( groups => {cb(groups)})
+}
 
-      Groepen.findAll({
-        where: {EigenaarsID: user},
-        attributes: ["ID","Naam"],
+function getOwnerGroups(user, cb){
+  Groepen.findAll({
+    where: {EigenaarsID: user},
+    attributes: ["ID","Naam"],
 
-        include: [
-          {
-            limit: 1,
-            model: Berichten,
-            as: 'berichten',
-            attributes: ['Tijd'],
-            order: [[ 'Tijd', 'DESC' ]]
-          }
-        ]
+    include: [
+      {
+        limit: 1,
+        model: Berichten,
+        as: 'berichten',
+        attributes: ['Tijd'],
+        order: [[ 'Tijd', 'DESC' ]]
+      }
+    ]
+  })
+    .then( groups => {cb(groups)})
+}
+
+function getGroupList( user, cb ){
+
+  var i,j, output = []
+
+  getMemberGroups(user,groups=>{
+    for(i=0; i<groups.length; i++) output[i] = groups[i]
+
+    getOwnerGroups(user,groups=>{
+      for(j=0; j<groups.length; j++) output[i+j] = groups[j]
+        output.sort((a,b)=>{
+          if(a.berichten[0]&&b.berichten[0]&&a.berichten[0].dataValues.Tijd<b.berichten[0].dataValues.Tijd)return 1
+          else if(b.berichten[0]) return 1
+          return -1
+        })
+        cb(output)
       })
-        .then( groups => {
-          for(j=0;j<groups.length;j++) output[i+j] = groups[j];
-          output.sort((a,b)=>{
-            if(a.berichten[0]&&b.berichten[0]&&a.berichten[0].dataValues.Tijd<b.berichten[0].dataValues.Tijd)return 1;
-            else return -1;
-          });
-          cb(output);
-        });
-    });
+    })
+}
+
+function getMemberList(user,groupID,cb){
+
+  var i,output = []
+
+  getGroupList(user,list=>{
+    for(var item of list)if(item.ID==groupID){
+      Groepen.findOne({
+        where: { ID: groupID }
+      })
+        .then(group=>{
+          output[0] = group.dataValues.EigenaarsID
+
+      GroepsLeden.findAll({ where: {GroepsID: groupID}})
+        .then(groups=>{
+          for(i=0; i<groups.length; i++)output[i+1]=groups[i].dataValues.GebruikersID
+          cb(output)
+        })})
+    }
+  })
 }
 
 // create a sequelize instance with our local mysql database information.
 function findUser(user,pass,cb){
   Gebruikers.findOne({ where: { ID: user, Wachtwoord: pass}}).then(user=>{
-    if(user)cb(true);
-    else cb(false);
-  });
+    if(user)cb(true)
+    else cb(false)
+  })
 }
 
 function getGroupMessages(group,cb){
@@ -127,18 +157,18 @@ function saveMessage(text,group,user,cb){
       Bericht: text,
       AuteursID: user,
       GroepsID: group
-    }).then(()=>{cb(false)});
+    }).then(()=>{cb(false)})
 }
 
 function getGroup(group,cb){
-  Groepen.findOne({ where: { ID: group }}).then((group)=>{cb(group)});
+  Groepen.findOne({ where: { ID: group }}).then((group)=>{cb(group)})
 }
 
 function addGroup(user,name){
   Groepen.create({
     Naam: name,
     EigenaarsID: user
-  });
+  })
 }
 
 function addGroupMember(owner,user,group,isAdmin){
@@ -149,17 +179,23 @@ function addGroupMember(owner,user,group,isAdmin){
         GroepsID: group,
         isBeheerder: isAdmin
       }).catch(err=>{
-        console.log(err);
-      });
+        console.log(err)
+      })
     }
-  });
+  })
 }
 
-exports.sequelize = sequelize;
-exports.findUser = findUser;
-exports.getGroupList = getGroupList;
-exports.getGroupMessages = getGroupMessages;
-exports.saveMessage = saveMessage;
-exports.getGroup = getGroup;
-exports.addGroup = addGroup;
-exports.addGroupMember = addGroupMember;
+function removeGroup(user,group){
+  Groepen.destroy({ where: { ID: group, EigenaarsID: user }})
+}
+
+exports.sequelize = sequelize
+exports.findUser = findUser
+exports.getGroupList = getGroupList
+exports.getGroupMessages = getGroupMessages
+exports.saveMessage = saveMessage
+exports.getGroup = getGroup
+exports.addGroup = addGroup
+exports.addGroupMember = addGroupMember
+exports.getMemberList = getMemberList
+exports.removeGroup = removeGroup
