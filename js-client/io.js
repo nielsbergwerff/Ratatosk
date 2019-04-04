bf2db8fd90058995da9ffc116e15d34b28d5d0c9
StriderOfTheWest <ntvangoor@gmail.com>
2019-04-04 21:37:19 +0200

    members zichtbaar in lijst; Groepenlijst update; Groep verwijderlistener

--- js-client/io.js
+++ js-client/io.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  var socket = io('http://localhost:80');
+  var socket = io('localhost:80');
 
   function setGroupButtonListener(){
     $('.setGroupButton').click(function(e){
@@ -18,30 +18,48 @@ $(function () {
   });
 
   $('#addMemberButton').click(function(e){
-    socket.emit('add group member',$('#addMember').val(),editGroup,0)
+    socket.emit('add member',$('#addMember').val(),editGroup,0)
   });
 
-  socket.on('set group list',(groupList)=>{
-    socket.emit('set group',groupList[0]);
-    $('#groupColumn').text('');
+  $('#deleteGroup').click(function(e){
+    socket.emit('delete group',editGroup)
+  })
+
+  socket.on('set group list',groupList=>{
+    socket.emit('set group',groupList[0])
+    $('#groupColumn').text('')
     for(var group of groupList)
-      $('#groupColumn').append($('<div id="'+group.ID+'">').html('<input type="button" class="groupSettings"/><p>'+group.Naam+'</p><img src="images/selectButton.png" class="setGroupButton"></img>'));
-    setGroupButtonListener();
-    setGroupSettingsListener();
-  });
+      $('#groupColumn').append($('<div id="'+group.ID+'">').html('<input type="button" class="groupSettings"/><p>'+group.Naam+'</p><img src="images/selectButton.png" class="setGroupButton"></img>'))
+    setGroupButtonListener()
+    setGroupSettingsListener()
+  })
 
-  socket.on('set group',(group)=>{
-    $('#chat').text('');
-    socket.emit('get messages');
-  });
+  socket.on('set member list',memberList=>{
+    $('#memberColumn').text('')
+    for(var member of memberList)$('#memberColumn').append($('<div>').text(member))
+  })
 
-  socket.on('get messages',(messages)=>{
+  socket.on('set group',group=>{
+    $('#chat').text('')
+    socket.emit('get messages')
+  })
+
+  socket.on('get messages',messages=>{
     for (var message of messages)
-      $('#chat').append($('<p>').text(message.AuteursID+": "+message.Bericht));
-  });
+      $('#chat').append($('<p>').text(message.AuteursID+": "+message.Bericht))
+  })
 
-  socket.on('chat message',(msg)=>{
-    $('#chat').append($('<p>').text(msg));
-  });
+  socket.on('chat message',msg=>{
+    $('#chat').append($('<p>').text(msg))
+  })
 
-});
+  socket.on('add member',user=>{
+    $('#memberColumn').append($('<div>').text(user))
+  })
+
+  socket.on('delete group',group=>{
+    $('#'+group).remove()
+    hideGroupSettings()
+  })
+
+})
