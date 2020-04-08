40721d202085378f65cbcd0d24e6ccfb37f8d605
StriderOfTheWest <ntvangoor@gmail.com>
2020-04-08 22:04:26 +0200

    config.json wordt niet overschreven als hij al bestaat

--- INSTALL SERVER.bat
+++ INSTALL SERVER.bat
@@ -1,16 +1,20 @@
+@ECHO OFF
+
 MD js-server\models
 COPY NUL js-server\models\berichten.js
 COPY NUL js-server\models\gebruikers.js
 COPY NUL js-server\models\groepen.js
 COPY NUL js-server\models\groepsleden.js
 
-ECHO {>config.json
-ECHO  "port": ,>>config.json
-ECHO  "databaseHost": "",>>config.json
-ECHO  "databasePort": ,>>config.json
-ECHO  "user": "",>>config.json
-ECHO  "password": "",>>config.json
-ECHO  "database": "">>config.json
-ECHO }>>config.json
+IF NOT EXIST config.json (
+	ECHO {>config.json
+	ECHO  "port": ,>>config.json
+	ECHO  "databaseHost": "",>>config.json
+	ECHO  "databasePort": ,>>config.json
+	ECHO  "user": "",>>config.json
+	ECHO  "password": "",>>config.json
+	ECHO  "database": "">>config.json
+	ECHO }>>config.json
+)
 
 CALL npm i
