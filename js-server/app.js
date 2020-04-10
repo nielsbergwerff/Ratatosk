5730320f6aa7eb21c46b31c18f182e77c4f2f405
StriderOfTheWest <ntvangoor@gmail.com>
2020-04-10 17:18:23 +0200

    app gescheiden van server; dit is meer volgens conventie

---
+++ js-server/app.js
@@ -0,0 +1,90 @@
+const cookieParser = require('cookie-parser'),
+      createError = require('http-errors'),
+      express = require('express'),
+      helmet = require('helmet'),
+      logger = require('morgan'),
+      path = require('path'),
+
+      router = require('./routing'),
+
+      app = express()
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'))
+//app.set('view engine', 'pug')
+
+// protection
+app.use(helmet({
+  contentSecurityPolicy: { directives: { defaultSrc: ["'self'"] }},
+  permittedCrossDomainPolicies: true,
+  referrerPolicy: { policy: 'no-referrer' },
+  featurePolicy: {
+    features: {
+      accelerometer: ["'none'"],
+      ambientLightSensor: ["'none'"],
+      autoplay: ["'none'"],
+      camera: ["'none'"],
+      documentDomain: ["'none'"],
+      documentWrite: ["'none'"],
+      encryptedMedia: ["'none'"],
+      fontDisplayLateSwap: ["'none'"],
+      fullscreen: ["'none'"],
+      geolocation: ["'none'"],
+      gyroscope: ["'none'"],
+      layoutAnimations: ["'none'"],
+      legacyImageFormats: ["'none'"],
+      loadingFrameDefaultEager: ["'none'"],
+      magnetometer: ["'none'"],
+      microphone: ["'none'"],
+      midi: ["'none'"],
+      oversizedImages: ["'none'"],
+      payment: ["'none'"],
+      pictureInPicture: ["'none'"],
+      serial: ["'none'"],
+      speaker: ["'none'"],
+      syncScript: ["'none'"],
+      syncXhr: ["'none'"],
+      unoptimizedImages: ["'none'"],
+      unoptimizedLosslessImages: ["'none'"],
+      unoptimizedLossyImages: ["'none'"],
+      unsizedMedia: ["'none'"],
+      usb: ["'none'"],
+      verticalScroll: ["'none'"],
+      vibrate: ["'none'"],
+      vr: ["'none'"],
+      wakeLock: ["'none'"],
+      xr: ["'none'"]
+    }
+  }
+}))
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// Alternatively could be done in a single line, but that requires moving files
+app.use('/js', express.static('../js-client'))
+app.use('/css', express.static('../css'))
+app.use('/images', express.static('../images'))
+
+app.use('/', router)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  //res.status(err.status || 500);
+  //res.render('error');
+  next()
+});
+
+module.exports = app;
--- js-server/server.js
+++ js-server/server.js
@@ -1,78 +1,62 @@
-//Toelichting is gegeven op een basis niveau zodat mensen zonder kennis van Node-JS zich nog een beetje wegwijs kunnen maken
-//Hieronder staan alle gebruikte modules(open-source) van dit bestand
 const express = require('express'),
-      app = require('express')(),
+      app = require('./app'),
       http = require('http').Server(app),
       io = require('socket.io')(http),
       bodyParser = require("body-parser"),
-      helmet = require('helmet'),
       morgan = require('morgan'),
       cookieParser = require('cookie-parser'),
       expressSession = require('express-session'),
       connectSessionSequelize = require('connect-session-sequelize'),
-      //Eigen module
-      db = require('./db')
 
-//Configuratie waardes worden geïmporteerd
-const config = require('../config.json')
+      db = require('./db'),
+      config = require('../config.json'),
 
-//De session-opties worden gespecificeerd
-const session = expressSession({
-  //session naam
-  key: 'user_sid',
-  //session versleutelings sleutel
-  secret: 'ns&10Kd9;',
-  //plek voor de session om data op te slaan
-  store: new (connectSessionSequelize(expressSession.Store))({
-    //database wordt mee verbonden in db.js
-    db: db.sequelize
-  }),
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    //aan te zetten bij distributie
-    //secure: true,
-    //tijd dat de cookie geldig is
-    expires: 16 * 60 * 60 * 1000
-  }
-})
+      development = true,
 
-//echte folders worden toegewezen aan virtuele folders
-app.use('/js',express.static('../js-client'))
-app.use('/css',express.static('../css'))
-app.use('/images',express.static('../images'))
-//post data wordt op req.body gezet
-app.use(bodyParser.urlencoded({ extended: false }))
-//bescherming
-app.use(helmet())
-//logs
-app.use(morgan('dev'))
-app.use(cookieParser())
+      session = expressSession({
+        // session name
+        key: 'user_sid',
+        // session hash salt
+        secret: 'ns&10Kd9;',
+        store: new (connectSessionSequelize(expressSession.Store))({
+          // database to store session data
+          db: db.sequelize
+        }),
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          httpOnly: true,
+          secure: !development,
+          expires: 16 * 60 * 60 * 1000
+        }
+      })
+
+// echte folders worden toegewezen aan virtuele folders
+// bescherming
 app.use(session)
 
-//andere eigen modules die nu pas gebruikt moeten worden
+// andere eigen modules die nu pas gebruikt moeten worden
 const routing = require('./routing')(app,db),
       myIo = require('./io')(io,db)
 
-//zo cookie op moeten ruimen als de session verlopen is
-//NIET GETEST
+// zo cookie op moeten ruimen als de session verlopen is
+// NIET GETEST
 app.use((req, res, next)=>{
-  if (req.cookies.user_sid && !req.session.user)res.clearCookie('user_sid')
+  if (req.cookies.user_sid && !req.session.user) res.clearCookie('user_sid')
   next()
 })
 
-//verbindt socket.io met sessie
+// verbindt socket.io met sessie
 io.use((socket, next)=>{
     session(socket.request, socket.request.res, next)
 });
 
-//de server zoekt naar requests van clients op de ingestelde port
+// de server zoekt naar requests van clients op de ingestelde port
 http.listen(config.port, ()=>{
   console.log(`listening on *:${config.port}`)
 })
 
-//stuurt 404.html bij een 404-error
-app.use(function (req, res, next) {
-  res.status(404).sendFile('404.html',{root:'../html'})
+// stuurt 404.html bij een 404-error
+app.use(function (req, res) {
+  res.status(404).sendFile('404.html', {root:'../html'})
 })
